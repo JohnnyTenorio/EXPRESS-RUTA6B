3dcf851b9ec7f19ce273e862768c132be2554ee3
Johnny Tenorio <tenoriojhonny33@gmail.com>
2025-10-08 21:43:26 -0500

    SUBIDO POR JOHNNY TENORIO DE 6B TIC

---
+++ EXPRESS-RUTA/server/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import indexRoutes from '../routes/index.routes.js';
+
+export default class Server {
+    constructor() {
+        this.app = express();
+        this.port = 3000;
+        this.expressruta = '/api/';
+
+        //Middlewars
+        this.middlewares();
+
+        this.routes();
+
+    }
+
+    middlewares() {
+        this.app.use(cors());
+
+        this.app.use(express.json());
+
+        this.app.use(express.static('public'));
+
+    }
+
+    routes() {
+        //localhost:000/api/ejemplo
+        this.app.use(this.expressruta, indexRoutes);
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port);
+        })
+    }
+}
